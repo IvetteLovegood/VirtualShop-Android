893b97ccf318b52681049ca166a98fbfd1d630e4
Ivette Guadalupe Hernández Pérez <ivette.hernandez@yaxche.mx>
2019-08-07 19:32:09 -0500

    Se agrega estructura, vistas y activity de login y registro.

---
+++ app/src/main/java/com/ivettehernandez/virtual_shop/LoginActivity.kt
@@ -0,0 +1,16 @@
+package com.ivettehernandez.virtual_shop
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * Created by Ivette Hernández on 2019-08-07.
+ */
+class LoginActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+
+}
